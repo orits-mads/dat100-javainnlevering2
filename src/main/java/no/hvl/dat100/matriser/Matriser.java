5f942e40e63fa8c2396d4698db3f03b697ff2ae6
orits-mads <mads.oritsland@gmail.com>
2024-10-01 15:40:11 +0200

    Update Matriser.java

--- src/main/java/no/hvl/dat100/matriser/Matriser.java
+++ src/main/java/no/hvl/dat100/matriser/Matriser.java
@@ -10,19 +10,26 @@ public class Matriser {
 			{7, 8, 9}
 		};
 
+		System.out.println("Skriv ut matrise1 som en matrise:");
 		skrivUt(matrise1);
+
+		System.out.println();
+		System.out.println("Skriv ut matrise1 som en streng:");
+		System.out.println(tilStreng(matrise1));
+
+		System.out.println("Skriver ut matrisen skalert med 5:");
+		skrivUt(skaler(5, matrise1));
 	}
 
 	// a)
 	public static void skrivUt(int[][] matrise) {
 		
 		for( int[] m : matrise) {
-			System.out.println();
 			for( int s : m){
 				System.out.print(s + " ");
 
 			}
-
+			System.out.println();
 		}
 
 
@@ -31,17 +38,34 @@ public class Matriser {
 	// b)
 	public static String tilStreng(int[][] matrise) {
 
-		// TODO
-		throw new UnsupportedOperationException("Metoden tilStreng ikke implementert");
-		
+		String str = "";
+
+		for (int[] m : matrise){
+			for (int s : m){
+				str += (s + " ");
+			}
+			str += "\n";
+		}
+
+		return str;
 	}
 
 	// c)
 	public static int[][] skaler(int tall, int[][] matrise) {
 		
-		// TODO
-		throw new UnsupportedOperationException("Metoden skaler ikke implementert");
-	
+		int[][] mat = new int[matrise.length][matrise[0].length];
+		int m = 0;
+
+		for (int[] rad : matrise){
+			int n = 0;
+			for ( int s : rad){	
+				mat[m][n] = matrise[m][n] * tall;
+				n++;
+			}
+			m++;
+		}
+
+		return mat;
 	}
 
 	// d)
